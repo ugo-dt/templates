1aead9981963ab9d02b5f2070581dedb08c50af5
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-09-22 15:55:20 +0200

    macos compatible

--- script.sh
+++ script.sh
@@ -2,26 +2,30 @@
 
 set -e
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+if [ $(uname) == "Linux" ]; then
+	sudo apt-get -y update
+	sudo apt-get -y upgrade
 
-sudo apt-get install -y clang \
-                        git \
-                        curl \
-                        make \
-                        python3 \
-                        pip \
-                        vim \
-                        zsh
+	sudo apt-get install -y clang \
+							git \
+							curl \
+            	            make \
+							python3 \
+							pip \
+							vim \
+							zsh
+	# Norminette
+	python3 -m pip install --upgrade pip setuptools
+	python3 -m pip install norminette
+else
+	brew update
+	brew upgrade
+fi
 
 # Oh My Zsh
 if [ ! -d "$ZSH" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
-# Norminette
-python3 -m pip install --upgrade pip setuptools
-python3 -m pip install norminette
 
 zsh -c "source ~/.zshrc && alias norminette" &> /dev/null
 if [ $? -ne 0 ]; then
@@ -35,15 +39,15 @@ if [ ! -d ~/.vim/pack/plugins/start/vim-polyglot ]; then
 fi
 
 # .vimrc
-wget -O ~/.vimrc https://raw.githubusercontent.com/ugo-dt/templates/main/.vimrc
+wget -O ~/.vimrc https://raw.githubusercontent.com/ugo-dt/templates/main/.vimrc --no-check-certificate
 
 # 42 Header
-wget -O ~/.vim/plugin/stdheader.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/plugin/stdheader.vim
+wget -O ~/.vim/plugin/stdheader.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/plugin/stdheader.vim --no-check-certificate
 if [ $(echo -n "$MAIL" | wc -m) -eq 0 ]; then
     echo 'export MAIL=$USER@student.42.fr' >> ~/.zshrc
 fi
 
 # Onedark vim theme
-wget -O ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/autoload/onedark.vim
-wget -O ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/colors/onedark.vim
+wget -O ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/autoload/onedark.vim --no-check-certificate
+wget -O ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/colors/onedark.vim --no-check-certificate
 ### echo '\n" onedark.vim theme\ncolorscheme onedark' >> ~/.vimrc
