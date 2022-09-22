a37d08a0f01a378e4904953c2604f07bf4ea6324
ugo-dt <ugdaniel@student.42.fr>
2022-09-22 14:40:16 +0200

    new links to repo

--- script.sh
+++ script.sh
@@ -22,23 +22,28 @@ fi
 # Norminette
 python3 -m pip install --upgrade pip setuptools
 python3 -m pip install norminette
-echo 'alias norminette="python3 -m norminette' >> ~/.zshrc
+
+zsh -c "source ~/.zshrc && alias norminette" &> /dev/null
+if [ $? -ne 0 ]; then
+    echo 'alias norminette="python3 -m norminette' >> ~/.zshrc
+fi
 
 # Vim
 mkdir -p ~/.vim/plugin ~/.vim/colors/ ~/.vim/autoload/
+if [ ! -d ~/.vim/pack/plugins/start/vim-polyglot ]; then
+    git clone https://github.com/sheerun/vim-polyglot.git ~/.vim/pack/plugins/start/vim-polyglot
+fi
 
 # .vimrc
-wget -O ~/.vimrc https://raw.githubusercontent.com/LexouDuck/42/master/.vimrc
+wget -O ~/.vimrc https://raw.githubusercontent.com/ugo-dt/templates/main/.vimrc
 
 # 42 Header
-wget -O ~/.vim/plugin/stdheader.vim https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim
+wget -O ~/.vim/plugin/stdheader.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/plugin/stdheader.vim
 if [ $(echo -n "$MAIL" | wc -m) -eq 0 ]; then
     echo 'export MAIL=$USER@student.42.fr' >> ~/.zshrc
 fi
 
 # Onedark vim theme
-wget -O ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/main/autoload/onedark.vim
-wget -O ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/main/colors/onedark.vim
-echo '\n" onedark.vim theme\ncolorscheme onedark' >> ~/.vimrc
-
-. ~/.zshrc
+wget -O ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/autoload/onedark.vim
+wget -O ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/ugo-dt/templates/main/.vim/colors/onedark.vim
+### echo '\n" onedark.vim theme\ncolorscheme onedark' >> ~/.vimrc
